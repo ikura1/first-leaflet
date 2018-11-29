760a4dd9d6a3e289ae0cb211f4f5e12d3ea730cb
ikura1 <ikura.lab@gmail.com>
2018-11-29 21:20:55 +0900

    doc: comment追加

--- base.js
+++ base.js
@@ -9,16 +9,20 @@ function get_json(url) {
 let mymap = L.map("mapid", {
   // Map初期値
   center: [34.691695541638275, 135.19788582261447],
-  // center: [42.7450, 141.9123],
+  // PluginのPanコントロールのオンオフ
   panControl: true,
+  // 初期のZoom値
   zoom: 16,
+  // Zoomの最小最大
   minZoom: 2,
   maxZoom: 18,
+  // Pluginのスライダーのオンオフ
   zoomsliderControl: true,
+  // デフォルトのZoomコントロールのオンオフ
   zoomControl: false
 });
-// layer追加まで行くと出力される
 
+// mapbox
 let mapbox = L.tileLayer(
   "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
   {
@@ -30,7 +34,10 @@ let mapbox = L.tileLayer(
       "pk.eyJ1IjoiaWt1cmExIiwiYSI6ImNqbWFxYjJ6azBkMWIzd3IxYmZ2bXowbHAifQ.RmON_TolsCqKnyOamXEGCQ"
   }
 );
+
+// osm
 let osm = L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+  // 権利関係?右したとかに出る
   attribution:
     '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 });
@@ -38,8 +45,7 @@ let osm = L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
 let polygon_json = get_json("geojson/9001reproject.geojson");
 let polygon_layer = L.geoJson(polygon_json, {
   onEachFeature: function(feat, layer) {
-    // 1. geojsonのpropertiesから情報を取得(座標系回りや渡す座標を考える必要がある)
-    // 2. APIに座標を投げる
+    // 地物をクリックしたら、起る動作
     const columns = ["遺跡名", "区番号", "所在地", "時代", "種類", "備考"];
     const headers = columns.map(value => "<th>" + value + "</th>").join("");
 
@@ -56,15 +62,18 @@ let polygon_layer = L.geoJson(polygon_json, {
 let point_json = get_json("./geojson/9003reproject.geojson");
 let point_layer = L.geoJson(point_json, {}).addTo(mymap);
 
+// 同時に表示できるLayer
 let Map_o = {
   polygon: polygon_layer,
   point: point_layer
 };
 // markerClusterGroup グループレイヤー登録ができる?
+// 同時に表示できないLayer(背景)
 let Map_b = {
   MapBox: mapbox,
   OpenStreetMap: osm
 };
 L.control.scale({ maxWidth: 250, imperial: false }).addTo(mymap);
+
 L.control.layers(Map_b, Map_o, { collapsed: false }).addTo(mymap);
 let hash = L.hash(mymap);
